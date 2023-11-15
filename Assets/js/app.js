09c0e1471e92357f405c4efb1d38283141db5765
UtasiB <utasibalazs@turr.hu>
2023-11-15 10:34:52 +0100

    Fixek

--- Assets/js/app.js
+++ Assets/js/app.js
@@ -1,15 +1,14 @@
-const ServerURL = "http://localhost:5000/"
+const serverURL = 'http://localhost:5000';
 
-async function render(view)
-{
+async function render(view){
     let main = document.querySelector('main');
     main.innerHTML = await (await fetch(`Views/${view}.html`)).text();
 }
 
-function alertBox(msg)
-{
-    let alertbox = document.querySelectorAll('#alertBox')
-    alertbox.innerHTML = `<strong>HIBA!</strong> ${msg}`
-    alertbox.classList.remove('d-none')
+function showMessage(msg){
+    let alertBox = document.querySelector('#alertBox');
+    alertBox.innerHTML = `<strong>HIBA!</strong> ${msg}`;
+    alertBox.classList.remove('d-none');
 }
+
 render('login')
---
+++ Controllers/login.js
@@ -0,0 +1,28 @@
+function login(){
+    let email = document.querySelector('#email');
+    let passwd = document.querySelector('#jelszo');
+
+    if (email.value == "" || passwd.value == "" ){
+        showMessage("Nem adtál meg minden adatot!");
+    }
+    else{
+        let data = {
+            email: email.value,
+            passwd: passwd.value
+        }
+        axios.post(`${serverURL}/logincheck`, data).then(res => {
+            if(res.data.length == 0){
+                showMessage("Hibás belépési adatok!");
+            }else
+            {
+                sessionStorage.setItem('MoneyManagerAppUser', JSON.stringify(res.data[0]));
+                document.location.href='index.html';
+            }
+        })
+    }
+}
+
+function logout(){
+    sessionStorage.removeItem('MoneyManagerAppUser');
+    document.location.href='index.html';
+}
